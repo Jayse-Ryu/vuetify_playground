e0fd11488c67e603cd6e65b7754c7edc8ab872ae
Dong-geun Ryu <dgkim723@hotmail.com>
2020-02-19 17:23:52 +0900

    Update - Keep testing..

--- src/views/dashboard/Header.vue
+++ src/views/dashboard/Header.vue
@@ -1,11 +1,7 @@
 <template>
-  <v-app
-    id="inspire"
-    dark
-  >
+  <v-app id="header">
     <v-navigation-drawer
       v-model="drawer"
-      dark
       :clipped="$vuetify.breakpoint.lgAndUp"
       app
     >
@@ -99,7 +95,7 @@
     <v-app-bar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
       app
-      color="red darken-4"
+      color="blue darken-3"
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
@@ -139,160 +135,12 @@
         </v-avatar>
       </v-btn>
     </v-app-bar>
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-tooltip right>
-            <template v-slot:activator="{on}">
-              <v-btn
-                :href="source"
-                icon
-                large
-                target="_blank"
-                v-on="on"
-              >
-                <v-icon large>
-                  mdi-code-tags
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Source</span>
-          </v-tooltip>
-          <!--<v-tooltip right>
-            <template v-slot:activator="{on}">
-              <v-btn
-                icon
-                large
-                href="https://codepen.io/johnjleider/pen/MNYLdL"
-                target="_blank"
-                v-on="on"
-              >
-                <v-icon large>
-                  mdi-codepen
-                </v-icon>
-              </v-btn>
-            </template>
-            <span>Codepen</span>
-          </v-tooltip>-->
-          Vuetify study
-        </v-row>
-      </v-container>
-    </v-content>
-    <v-btn
-      bottom
-      color="pink"
-      dark
-      fab
-      fixed
-      right
-      @click="dialog = !dialog"
-    >
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="800px"
-    >
-      <v-card>
-        <v-card-title class="grey darken-2">
-          Create contact
-        </v-card-title>
-        <v-container>
-          <v-row class="mx-2">
-            <v-col
-              class="align-center justify-space-between"
-              cols="12"
-            >
-              <v-row
-                align="center"
-                class="mr-0"
-              >
-                <v-avatar
-                  size="40px"
-                  class="mx-3"
-                >
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  />
-                </v-avatar>
-                <v-text-field placeholder="Name" />
-              </v-row>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                prepend-icon="mdi-account-card-details-outline"
-                placeholder="Company"
-              />
-            </v-col>
-            <v-col cols="6">
-              <v-text-field placeholder="Job title" />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-mail"
-                placeholder="Email"
-              />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                type="tel"
-                prepend-icon="mdi-phone"
-                placeholder="(000) 000 - 0000"
-              />
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-text"
-                placeholder="Notes"
-              />
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-btn
-            text
-            color="primary"
-          >
-            More
-          </v-btn>
-          <v-spacer />
-          <v-btn
-            text
-            color="primary"
-            @click="dialog = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            text
-            @click="dialog = false"
-          >
-            Save
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-app>
 </template>
 
 <script>
 export default {
-  props: {
-    source: {
-      type: String,
-      default: null,
-    },
-  },
-
   data: () => ({
-    dialog: false,
     drawer: null,
     items: [
       { icon: 'mdi-contacts', text: 'Contacts', cta: 'about' },
@@ -327,3 +175,9 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+  #header {
+    //
+  }
+</style>
